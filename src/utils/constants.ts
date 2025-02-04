085efb37bb117afc3c0e10a8a64bfe342f2a4915
Cipher | Vansh <ciphrox@gmail.com>
2025-02-04 20:58:45 +0530

    updated menu bar to become collapsable

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,6 @@
+import PlaceholderIcon from "@/assets/icons/placeholder.svg";
+import HomeIcon from "@/assets/icons/home.svg";
+import RecordIcon from "@/assets/icons/record.svg";
 import type { LinkProps } from "@/types";
 
 export const APP_NAME = "Template";
@@ -7,48 +10,56 @@ export const NavbarLinks: LinkProps[] = [
     name: "Home",
     title: "Home",
     path: "/",
+    icon: HomeIcon,
     isActive: true
   },
   {
     name: "About",
     title: "About",
     path: "/about",
+    icon: PlaceholderIcon,
     isActive: true
   },
   {
     name: "Varnmala",
     title: "Varnmala",
     path: "/varnmala",
+    icon: PlaceholderIcon,
     isActive: true
   },
   {
     name: "Panel",
     title: "Panel",
     path: "/panel",
+    icon: PlaceholderIcon,
     isActive: true
   },
   {
     name: "Reader",
     title: "Reader",
     path: "/reader",
+    icon: PlaceholderIcon,
     isActive: true
   },
   {
     name: "Record",
     title: "Record",
     path: "/record",
+    icon: RecordIcon,
     isActive: true
   },
   {
     name: "Hindi",
     title: "Hindi",
     path: "/hindi",
+    icon: PlaceholderIcon,
     isActive: true
   },
   {
     name: "Typing",
     title: "Typing",
     path: "/typing",
+    icon: PlaceholderIcon,
     isActive: true
   }
 ];
