fd570a2e7c9142620cc901f6f835d12f5e48856a
Cipher | Vansh <ciphrox@gmail.com>
2025-02-19 23:03:12 +0530

    merged letter page to english

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -68,12 +68,6 @@ export const NavbarLinks: LinkProps[] = [
     isActive: true
   },
   {
-    name: "Letter",
-    title: "Letter Pad",
-    path: "/letter",
-    isActive: true
-  },
-  {
     name: "Feedback",
     title: "Feedback Form",
     path: "/feedback",
