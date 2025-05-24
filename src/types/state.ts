e7175204e861bcc56e99f116e374f4ceb95d7266
Suresh-Chelani <sureshc.btech2022@iujaipur.edu.in>
2025-05-24 17:12:34 +0530

    add responsiveness in state popup, change the ui of state info. page, done some alignment in palyground page

---
+++ src/types/state.ts
@@ -0,0 +1,18 @@
+export interface StateData {
+  id: string;
+  state_id: number;
+  name: string;
+  dance: string;
+  festival: string;
+  description: string;
+  image: string;
+  svg: string;
+  wikiLink: string;
+  official_website: string;
+  isActive: boolean;
+  alternate_dance?: string;
+  famous_quote?: string;
+  famous_person?: string;
+  famous_food?: string;
+  famous_place?: string;
+}
