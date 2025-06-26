23b7dea1c70d05f966d3c9b04c94d4990740af46
xkeshav <xkeshav@gmail.com>
2025-06-27 02:19:01 +0530

    [RZB-250021]: remove duplicate css from numbers and alphabets section

--- src/types/alphabet.ts
+++ src/types/alphabet.ts
@@ -6,9 +6,9 @@ export type LetterEntity = {
 export interface AlphabetEntry {
   id: number | string;
   letter: string;
-  code?: number;
   text: string;
   description: string;
+  code?: number;
   emoji?: string;
   image?: string;
   englishSound?: string;
@@ -43,3 +43,23 @@ export interface GeekAlphabetEntry {
   name: string;
   representations: string[];
 }
+
+export type CardSectionType = {
+  icon?: string;
+  id?: string;
+  kind?: string;
+  title: string;
+  description: string;
+  href: {
+    playground: {
+      link: string;
+      icon: string;
+      text: string;
+    };
+    board?: {
+      link: string;
+      icon: string;
+      text: string;
+    };
+  };
+};
