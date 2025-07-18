280c958d6f0bfdee97e9d536ce1709d60164eaea
Rupesh-Rathore <rupesh010903@gmail.com>
2025-07-18 21:49:54 +0530

    new page poem

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -52,3 +52,15 @@ export const getInitials = (name: string): string => {
 
   return initials;
 };
+
+export const sortBy = (arr:any, key:string)=>{
+return  arr.sort((a:any,b:any)=>{
+  if(a[key] < b[key]){
+    return -1;
+  }
+  if(a[key] > b[key]){
+    return 1;
+  }
+  return 0;
+});
+}
