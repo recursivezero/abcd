801654487688130783f4e8a2bfcb081bf47e53d8
recursivezero <recursivezero@outlook.com>
2025-07-20 01:43:17 +0530

    [RZE-2500001]: added index page on /

--- backend/src/index.tsx
+++ backend/src/index.tsx
@@ -2,7 +2,10 @@ import { serve } from "@hono/node-server";
 import { swaggerUI } from "@hono/swagger-ui";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import dotenv from "dotenv";
+import { Context } from "hono";
 import { cors } from "hono/cors";
+import { Style, css } from 'hono/css';
+
 import { connectDB } from "./config/db";
 import subscriptionRouter from "./routes/subscription";
 import userRoutes from "./routes/user";
@@ -14,6 +17,39 @@ app.use(cors());
 
 app.route("/", userRoutes);
 app.route("/", subscriptionRouter);
+
+const View = () => {
+  const headerClass = css`
+    background-color: green;
+    color: white;
+    padding: 1rem;
+    text-align: center;
+  `;
+  const divClass = css`
+    display:grid;
+    place-content: center;
+    & a {
+      color:red;
+      font-size: 2rem;
+    }
+    `;
+  return (
+    <html>
+      <head><Style /></head>
+      <body>
+        <h1 class={ headerClass }>Hello Hono!</h1>
+        <div class={ divClass }>
+          <a href="/docs">Docs</a>
+        </div>
+      </body>
+    </html>
+  )
+};
+
+
+app.get("/", (c: Context) => {
+  return c.html(<View />)
+});
 
 app.doc("/openapi.json", {
   openapi: "3.0.0",
